0410762e27bc2af140a34ff9ac6f3679f5b89f2f
Adauto Barbosa Filho <147922224+adautobf@users.noreply.github.com>
2023-10-30 17:14:43 -0300

    Console log indicando modificações do Adauto

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+//mais um console de teste
+console.log("O Adauto fez esse commit sem você ver na aula!")
+
+
