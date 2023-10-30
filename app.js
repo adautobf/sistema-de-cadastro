8e30b34558cc7a9e63af6a6ab84217c239a9e020
Adauto Barbosa Filho <filho.barbo@gmail.com>
2023-10-30 17:21:43 -0300

    Adicionando um console.log

--- app.js
+++ app.js
@@ -2,5 +2,6 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 //mais um console de teste
 console.log("O Adauto fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
 
 
