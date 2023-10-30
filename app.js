941da3cc7e686d6997520579019b734f09da959c
Adauto Barbosa Filho <filho.barbo@gmail.com>
2023-10-30 17:38:12 -0300

    Voltando para o estado do Adauto

--- app.js
+++ app.js
@@ -2,6 +2,5 @@ console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 //mais um console de teste
 console.log("O Adauto fez esse commit sem você ver na aula!")
-console.log("Enviando para o GitHub")
 
 
